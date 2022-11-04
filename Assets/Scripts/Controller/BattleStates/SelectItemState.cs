6dbb3858e4dd3e9fe73244a26776fdcf28223008
kailuncai <kailuncai@gmail.com>
2022-11-04 22:45:31 +0100

    lista mochila introducción color rojo (para ver si se ha superado del máximo)

--- Assets/Scripts/Controller/BattleStates/SelectItemState.cs
+++ Assets/Scripts/Controller/BattleStates/SelectItemState.cs
@@ -18,14 +18,29 @@ public class SelectItemState : BattleState
         ItemSelectionUI.gameObject.SetActive(true);
 
         //Abilities[] a = owner.currentUnit.weapon.Abilities;
-        List<ConsumableSlot> a = owner.backpackInventory.consumableContainer;
+        List<ConsumableSlot> itemList = owner.backpackInventory.consumableContainer;
 
         for (int i = 0; i < owner.itemSelectionUI.options.Length; i++)
         {
+            var item = itemList[i];
+            ItemSelectionUI.options[i].GetComponent<Text>().text = item.consumable.ItemName;
 
-            ItemSelectionUI.options[i].GetComponent<Text>().text = a[i].consumable.ItemName;
-            ItemSelectionUI.itemAmountText[i].GetComponent<Text>().text = a[i].amount.ToString();
-            ItemSelectionUI.itemImage[i].GetComponent<Image>().sprite = a[i].consumable.sprite;
+            ItemSelectionUI.itemAmountText[i].GetComponent<Text>().text = item.amount.ToString();
+
+            if (item.amount == item.consumable.maxBackPackAmount)
+            {
+                ItemSelectionUI.itemAmountText[i].GetComponent<Text>().color = Color.green;
+
+            }
+            else if(item.amount < item.consumable.maxBackPackAmount)
+            {
+                ItemSelectionUI.itemAmountText[i].GetComponent<Text>().color = Color.black;
+            }
+            else if(item.amount > item.consumable.maxBackPackAmount)
+            {
+                ItemSelectionUI.itemAmountText[i].GetComponent<Text>().color = Color.red;
+            }
+            ItemSelectionUI.itemImage[i].GetComponent<Image>().sprite = item.consumable.sprite;
             //if (owner.currentUnit.stamina < a[i].staminaCost)
             //{
             //    AbilitySelectionUI.DisableSelectAbilty(i);
