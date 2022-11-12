4d9d0d96b27a0a60281d3bb4259dcb735ada946a
kailuncai <kailuncai@gmail.com>
2022-11-12 23:16:04 +0100

    Animaciones forja y tienda. Implementado perro

--- Assets/Scripts/UI/Camp/New Scripts/OpenNewTab.cs
+++ Assets/Scripts/UI/Camp/New Scripts/OpenNewTab.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class OpenNewTab : MonoBehaviour
+public class OpenNewTab : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] GameObject tabToOpen;
+    //[SerializeField] GameObject gameObjectAnimator;
+     public Animator animator;
 
-    private void Start()
-    {
-        Debug.Log("HOLA");
-    }
+    //private void Wake()
+    //{
+    //    animator = gameObjectAnimator.GetComponent<Animator>();
+    //}
     public void OpenTab()
     {
-        Debug.Log("pipi");
+     
         if(tabToOpen != null)
         {
             Debug.Log("GOLPEA");
@@ -23,4 +27,17 @@ public class OpenNewTab : MonoBehaviour
             Debug.Log("There's no tab to open");
         }
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("AAAAAAA");
+        animator.SetInteger("ToIdle", 1);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetInteger("ToIdle", 0);
+    }
+
+ 
 }
